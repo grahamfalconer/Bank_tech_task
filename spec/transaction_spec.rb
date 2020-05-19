2d7a4a5398db167dee8efbe2790146ee04d03ba2
Graham <grahamalconer@icloud.com>
2020-05-19 13:12:01 +0100

    Red: adding time created to transsction test, undefined method time_created

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -6,5 +6,9 @@ describe Transaction do
     transaction = Transaction.new(amount: 1000)
     expect(transaction.amount).to eq 1000
   end
+  it 'Has the time it was created' do
+    transaction = Transaction.new(amount: 1000)
+    expect(transaction.time_created).to eq Time.now
+  end
 
 end
