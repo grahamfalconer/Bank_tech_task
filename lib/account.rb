8a6d83c6311000abae58828c2f5757c01b31fe73
Graham <grahamalconer@icloud.com>
2020-05-19 12:11:50 +0100

    Green: storage for transaction added

--- lib/account.rb
+++ lib/account.rb
@@ -1,11 +1,12 @@
 class Account
 
-  attr_reader :balance
+  attr_reader :balance, :transactions
 
   MINIMUM_BALANCE = 0
 
   def initialize
     @balance = MINIMUM_BALANCE
+    @transactions = []
   end
 
 end
