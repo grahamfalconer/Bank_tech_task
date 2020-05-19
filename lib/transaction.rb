21b1869c8e83eddc20a39bb499548b7f0844d11a
Graham <grahamalconer@icloud.com>
2020-05-19 15:13:02 +0100

    Green, test passed, user deposits and .balance_after_transaction is added to transaction

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,10 +1,11 @@
 class Transaction
-    attr_reader :amount, :date_created, :type
+    attr_reader :amount, :date_created, :type, :balance_after_transaction
 
   def initialize(amount)
     @type = ''
     @amount = amount[:amount]
     @date_created = Time.now.strftime("%Y-%d-%m %H:%M:%S %Z")
+    @balance_after_transaction = ''
   end
 
 end
