12eb6d1779c74074795837d0ab784e11e4f8f8b8
Graham <grahamalconer@icloud.com>
2020-05-20 11:47:25 +0100

    Green: added format_transaction to Statement, Statement formats transactions

--- lib/statement.rb
+++ lib/statement.rb
@@ -4,4 +4,12 @@ class Statement
   def initialize
     @column_descriptions = 'date || credit || debit || balance'
   end
+
+  def format_transaction(transaction)
+    if transaction.type == 'Credit'
+        "#{transaction.date_created[0..9]} || #{transaction.amount} || || #{transaction.balance_after_transaction}"
+    else
+        "#{transaction.date_created[0..9]} || || #{transaction.amount}  || #{transaction.balance_after_transaction}"
+    end
+  end
 end
