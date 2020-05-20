6162f2cca146d340fc9e2c697866a75a43a367da
Graham <grahamalconer@icloud.com>
2020-05-20 12:55:26 +0100

    Red: Test added for print statement on account... and its a doozy

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,8 +1,9 @@
 class Statement
-  attr_reader :column_descriptions
+  attr_reader :column_descriptions, :full
 
   def initialize
     @column_descriptions = 'date || credit || debit || balance'
+    @full = [column_descriptions]
   end
 
   def format_transaction(transaction)
@@ -12,4 +13,24 @@ class Statement
         "#{transaction.date_created[0..9]} || || #{transaction.amount}  || #{transaction.balance_after_transaction}"
     end
   end
+
+  def prepare_statement(transactions)
+    puts @column_descriptions
+    transactions.each do |transaction|
+      puts format_transaction(transaction)
+    end
+  end
 end
+=begin
+  def print
+    @account.transactions.each do |transaction|
+        @full << format_transaction(transaction)
+    end
+    p @full
+  end
+
+end
+
+statement = Statement.new
+statement.print
+=end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -43,4 +43,16 @@ describe Account do
     account.withdrawl(500)
     expect(account.balance).to eq 0
   end
+
+  it 'can print a full statement of all transactions' do
+    account = Account.new
+    account.deposit(2000)
+    account.withdrawl(500)
+    example_statement = "date || credit || debit || balance
+    2020-20-05 || 2000 || || 2000
+    2020-20-05 || || 500  || 1500"
+    p account.print_statement
+    p account.print_statement
+    expect(account.print_statement).to eq example_statement
+  end
 end
