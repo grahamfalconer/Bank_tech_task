b8738f9d3af480efab9a3779b626616ab0a08336
Graham <grahamalconer@icloud.com>
2020-05-20 11:47:25 +0100

    Green: added format_transaction to Statement, Statement formats transactions

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -5,4 +5,12 @@ describe Statement do
     statement = Statement.new
     expect(statement.column_descriptions).to eq 'date || credit || debit || balance'
   end
+
+  it 'can format a transaction into statement format' do
+    account = Account.new
+    statement = Statement.new
+    account.deposit(2000)
+    transaction = account.latest_transaction
+    expect(statement.format_transaction(transaction)).to eq '2020-20-05 || 2000 || || 2000'
+  end
 end
